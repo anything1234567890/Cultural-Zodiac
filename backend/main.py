725ee3646508ad554c0e747408eb0047e7d00119
anything1234567890 <icchbin18@gmail.com>
2025-08-01 00:30:07 +0530

    Fix CORS for Netlify

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://cultural-zodiac.netlify.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
